df38eb27578e47e468a887c44b2ac1ccdb61ed77
Nikolay Kuznetsov <nikolay.kuznetsov.ru@gmail.com>
2025-04-03 17:11:48 +0300

    call openai

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/nikolayk812/sqlcpp/internal/template"
 	"github.com/tmc/langchaingo/llms"
-	"github.com/tmc/langchaingo/llms/ollama"
+	"github.com/tmc/langchaingo/llms/openai"
 	"log"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -15,20 +18,28 @@ func main() {
 
 	engine := template.NewEngine("internal/template/data/repository.tmpl", data)
 
-	output, err := engine.Execute()
+	request, err := engine.Execute()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	llm, err := ollama.New(
-		ollama.WithModel("llama3.2"),
-		ollama.WithServerURL("http://localhost:11434"))
+	//llm, err := ollama.New(
+	//	ollama.WithModel("llama3.2"),
+	//	ollama.WithServerURL("http://localhost:11434"))
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	llm, err := openai.New(
+		openai.WithModel("gpt-4o"),
+		openai.WithToken(os.Getenv("OPENAI_API_KEY")),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	content := []llms.MessageContent{
-		llms.TextParts(llms.ChatMessageTypeHuman, output),
+		llms.TextParts(llms.ChatMessageTypeHuman, request),
 	}
 
 	fmt.Println("calling LLM")
@@ -39,16 +50,43 @@ func main() {
 		log.Fatal(err)
 	}
 
+	var response strings.Builder
+
 	for _, choice := range completion.Choices {
 		if choice == nil {
 			continue
 		}
-		fmt.Println(choice.Content)
 
-		if choice.StopReason != "" {
+		response.WriteString(choice.Content)
+		// fmt.Println(choice.Content)
+
+		if choice.StopReason != "" && choice.StopReason != "stop" {
 			fmt.Println("Stop reason: ", choice.StopReason)
 		}
 	}
 
-	// log.Println(output)
+	codeResponse, err := extractGoCode(response.String())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print(codeResponse)
+}
+
+func extractGoCode(text string) (string, error) {
+	startMarker := "```go"
+	endMarker := "```"
+
+	start := strings.Index(text, startMarker)
+	if start == -1 {
+		return "", errors.New("start marker not found")
+	}
+	start += len(startMarker) // Move past the start marker
+
+	end := strings.Index(text[start:], endMarker)
+	if end == -1 {
+		return "", errors.New("end marker not found")
+	}
+
+	return text[start : start+end], nil
 }
